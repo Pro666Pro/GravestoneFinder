3868256c8b7028f9463155a999d659ec944d60f1
nexer1234 <77268699+Pro666Pro@users.noreply.github.com>
2024-10-18 21:01:56 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -2,6 +2,18 @@ if not game:IsLoaded() then
     game.Loaded:Wait()
 end
 
+if invis == true then
+wait(0.1)
+
+fireclickdetector(workspace.Lobby["Ghost"].ClickDetector)
+
+wait(0.2)
+
+game.ReplicatedStorage.Ghostinvisibilityactivated:FireServer()
+
+wait(0.1)
+end
+
 if game.Workspace:FindFirstChild("Gravestone") then
 game:GetService("StarterGui"):SetCore("SendNotification",{Title = "Warning",Text = "Gravestone Found" ,Duration = 10, Icon = "rbxthumb://type=Asset&id=9649923610&w=150&h=150",Button1 = "Nice"})
 end
