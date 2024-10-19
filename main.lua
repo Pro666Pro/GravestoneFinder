dab851305dd252d5bc9ba4d1481d5c7754664f62
nexer1234 <77268699+Pro666Pro@users.noreply.github.com>
2024-10-19 13:31:59 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -7,15 +7,31 @@ wait(0.1)
 
 fireclickdetector(workspace.Lobby["Ghost"].ClickDetector)
 
-wait(0.2)
+wait(0.1)
 
 game.ReplicatedStorage.Ghostinvisibilityactivated:FireServer()
 
-wait(0.1)
+wait(0.3)
 end
 
 if game.Workspace:FindFirstChild("Gravestone") then
 game:GetService("StarterGui"):SetCore("SendNotification",{Title = "Warning",Text = "Gravestone Found" ,Duration = 10, Icon = "rbxthumb://type=Asset&id=9649923610&w=150&h=150",Button1 = "Nice"})
 end
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Pro666Pro/serverhop/main/main.lua'))()
+
+wait(5)
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Pro666Pro/serverhop/main/main.lua'))()
+
+wait(5)
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Pro666Pro/serverhop/main/main.lua'))()
+
+wait(5)
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Pro666Pro/serverhop/main/main.lua'))()
+
+wait(5)
 
 loadstring(game:HttpGet('https://raw.githubusercontent.com/Pro666Pro/serverhop/main/main.lua'))()
