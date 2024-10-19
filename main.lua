1c27aacc0fbb6b7e4f5dca39ed0896a78561e512
nexer1234 <77268699+Pro666Pro@users.noreply.github.com>
2024-10-20 01:44:32 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -16,22 +16,24 @@ end
 
 if game.Workspace:FindFirstChild("Gravestone") then
 game:GetService("StarterGui"):SetCore("SendNotification",{Title = "Warning",Text = "Gravestone Found" ,Duration = 10, Icon = "rbxthumb://type=Asset&id=9649923610&w=150&h=150",Button1 = "Nice"})
+
+else
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Pro666Pro/serverhop/main/main.lua'))()
+
+wait(5)
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Pro666Pro/serverhop/main/main.lua'))()
+
+wait(5)
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Pro666Pro/serverhop/main/main.lua'))()
+
+wait(5)
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Pro666Pro/serverhop/main/main.lua'))()
+
+wait(5)
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Pro666Pro/serverhop/main/main.lua'))()
 end
-
-loadstring(game:HttpGet('https://raw.githubusercontent.com/Pro666Pro/serverhop/main/main.lua'))()
-
-wait(5)
-
-loadstring(game:HttpGet('https://raw.githubusercontent.com/Pro666Pro/serverhop/main/main.lua'))()
-
-wait(5)
-
-loadstring(game:HttpGet('https://raw.githubusercontent.com/Pro666Pro/serverhop/main/main.lua'))()
-
-wait(5)
-
-loadstring(game:HttpGet('https://raw.githubusercontent.com/Pro666Pro/serverhop/main/main.lua'))()
-
-wait(5)
-
-loadstring(game:HttpGet('https://raw.githubusercontent.com/Pro666Pro/serverhop/main/main.lua'))()
